fad42edf8ec4b33e3a801223478bc280e15eaee3
Calvin <calvinwang1002@gmail.com>
2024-03-01 18:39:06 -0500

    Initialized Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; // For navigating to different pages
+
+function Home() {
+    return(
+        <div>
+            <a>HHHHHHEEEEELLLLLOOOOOOO</a>
+        </div>
+    );
+}
+
+export default Home;
