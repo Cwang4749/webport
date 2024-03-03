8486e418889100c895ff291667037ffa8c790330
Calvin <calvinwang1002@gmail.com>
2024-03-02 20:08:35 -0500

    Added bubbles, asterisk, and asterisk animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; // For navigating to different pages
+import asterisk from "../images/asterisk.png"; // center image of home page
+import "./styles/Home.css"; // css for home page
 
 function Home() {
     return(
         <div>
-            <a>HHHHHHEEEEELLLLLOOOOOOO</a>
+            <span className="bubble" id="aboutme"></span>
+            <span className="bubble" id="portfolio"></span>
+            <span className="bubble" id="contact"></span>
+            <span className="bubble" id="experiences"></span>
+            <img className="asterisk" src={asterisk} />
         </div>
     );
 }
