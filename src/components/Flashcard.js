01c42bc505a0fd129df4882f155251530bec1ff3
Calvin <calvinwang1002@gmail.com>
2024-03-12 12:58:10 -0400

    Added animation to swtiching between flashcards by using a function instead of inline styling

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -2,20 +2,10 @@ import React, {useState} from "react";
 import "./styles/Flashcard.css";
 
 function Flashcard(props) {
+    const [flip, set_flip] = useState(false);
     
-    function FlipFlashCard() {
-        // if(props.active) {
-        //     props.setActive(false);
-        // }
-        // else {
-        //     props.setActive(true);
-        //     if(props.imageIndex == 0 || props.imageIndex == 2) {
-        //         props.setImageIndex(1);
-        //     }
-        //     else {
-        //         props.setImageIndex(2);
-        //     }
-        // }
+    function FlipFlashcard() {
+        set_flip(!flip);
         if(props.imageIndex + 1 == props.flashCardImage.length)
         {
             props.setImageIndex(-1);
@@ -25,10 +15,38 @@ function Flashcard(props) {
         }
     }
 
+    const FlashcardStyle = (if_text) => {
+        var rotation = 'rotateX(0deg)';
+        var card_opacity = '1';
+        var card_overflow = 'scroll';
+        if(flip) 
+        {
+            rotation = 'rotateX(180deg)';
+        }
+
+        // if it is not on the text card, then hide the text
+        if(props.imageIndex != -1 && if_text)
+        {
+            card_opacity =  0;
+            card_overflow = 'hidden';
+        }
+
+        // if it is not on the images, then hide the images
+        if(props.imageIndex == -1 && !if_text)
+        {
+            card_opacity =  0;
+        }
+        return {
+            transform : rotation,
+            opacity : card_opacity,
+            overflow : card_overflow
+        }
+    }
+
     return (
-        <div className="flashcard" onClick={() => FlipFlashCard()}>
-            <div className="flashcardtext" style={props.imageIndex != -1 ? {'opacity': '0', 'overflow': 'hidden'} : {'opacity' : '1'}}><div>{props.flashCardText.map((str,index) => {return(<p key={index}>{str} <br/></p> )})}</div></div>
-            <img loading="lazy" className="flashcardimg" style={props.imageIndex != -1 ? {'opacity': '1'} : {'opacity': '0'}} src={props.flashCardImage[props.imageIndex]} alt="loading..." />
+        <div className="flashcard" onClick={() => FlipFlashcard()} style={flip ? {'transform': 'rotateX(180deg)'} : {'transform' : 'rotateX(0deg)'}} >
+            <div className="flashcardtext" style={FlashcardStyle(true)}><div>{props.flashCardText.map((str,index) => {return(<p key={index}>{str} <br/></p> )})}</div></div>
+            <img loading="lazy" className="flashcardimg" style={FlashcardStyle(false)} src={props.flashCardImage[props.imageIndex]} alt="loading..." />
         </div>
     );
 }
